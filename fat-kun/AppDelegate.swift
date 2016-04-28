453205168473be5043ebab2e7f777494160a1d4b
Eiji Iwazawa <iwazawa@gmail.com>
2016-04-28 18:58:13 +0900

    Prototype

--- fat-kun/AppDelegate.swift
+++ fat-kun/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        fatten()
         return true
     }
 
@@ -41,6 +42,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    func fatten() {
+        let fm = NSFileManager.defaultManager()
+        let url = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("work")
+        do {
+            try fm.createDirectoryAtURL(url, withIntermediateDirectories: true, attributes: nil)
+        } catch {}
+    }
 }
 
--- fat-kun/ViewController.swift
+++ fat-kun/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let url = NSURL(fileURLWithPath: NSTemporaryDirectory()).URLByAppendingPathComponent("work").URLByAppendingPathComponent("fat-file.txt")
+    let fm = NSFileManager.defaultManager()
+    var data: NSData!
+
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let attributes = try! fm.attributesOfItemAtPath(url.path!)
+        let sizeAttr = attributes[NSFileSize] as! NSNumber
+        let size = sizeAttr.unsignedLongLongValue / 1024 / 1024
+        infoLabel.text = "\(size) MB"
+
         // Do any additional setup after loading the view, typically from a nib.
+        self.data = createMB().dataUsingEncoding(NSUTF8StringEncoding)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func createFile(sender: UIButton) {
+        fm.createFileAtPath(url.path!, contents:NSData(), attributes:nil)
+        let file = try! NSFileHandle(forWritingToURL: url)
+        var specify = Int(textField.text!)
+        if specify == nil {
+            specify = 10
+        }
+        for _ in 1...specify! {
+            file.writeData(self.data)
+        }
+        file.synchronizeFile()
+        file.closeFile()
+    }
 
+    @IBAction func deleteFile(sender: UIButton) {
+        try! fm.removeItemAtURL(url)
+        infoLabel.text = "0 byte"
+    }
+
+    func createMB() -> String {
+        var kb = ""
+        for _ in 1...1024 {
+            kb += "A"
+        }
+        var mb = ""
+        for _ in 1...1024 {
+            mb += kb
+        }
+        return mb
+    }
 }
 
